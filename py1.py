97abcaf0e3dc66b9cd5e72c844ba63c54b07c797
Hien Ho <henryho8012@outlook.com>
2020-07-18 23:39:45 -0500

    qsort2

--- py1.py
+++ py1.py
@@ -1,11 +1,31 @@
-def qsort(alist):
+def qsort1(alist):
     print(alist)
     if len(alist)<=1:
         return(alist)
     else:
         pivot = alist[0]
-        return qsort([x for x in alist[1:] if x<pivot]) + [pivot] + qsort([x for x in alist[1:] if x>=pivot])
+        return qsort1([x for x in alist[1:] if x<pivot]) + [pivot] + qsort1([x for x in alist[1:] if x>=pivot])
+
+def qsort2(alist,l,u):
+    print(alist)
+    if l>=u:
+        return (alist)
+    
+    m=l
+    for i in range(l+1,u+1):
+        if alist[i]<alist[l]:
+            m +=1
+            if i!=m:
+                alist[i],alist[m] = alist[m],alist[i]
+    
+    alist[l],alist[m] = alist[m],alist[i]
+    qsort2(alist,l,m-1)
+    qsort2(alist,m+1,u)
 
 
-test =['z','b','c','a']
-print(qsort(test))
+
+test =[6,3,1,8,4,7,2]
+#print(qsort1(test))
+print(test)
+print('')
+print(qsort2(test,0,len(test)-1))
